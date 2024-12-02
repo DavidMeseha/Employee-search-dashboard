90b597b889f6a531f29d493787c154d079dcdf4b
David <davidmmyh@gmail.com>
2024-12-02 18:34:48 +0200

    change constants for testing purbose

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -10,48 +10,48 @@ export const countries = [
 export const edu = [
   {
     degree: "Bachelor's degree",
-    count: 2
+    count: 0
   },
   {
     degree: "Master's degree",
-    count: 5
+    count: 0
   },
   {
     degree: "Doctoral degree",
-    count: 5
+    count: 2
   },
   {
     degree: "Executive-level",
-    count: 6
+    count: 0
   },
   {
     degree: "Counsultant-level",
-    count: 2
+    count: 0
   }
 ];
 
 export const yearsOfexp = [
   {
     name: "1-2",
-    count: 2,
+    count: 0,
     min: 1,
     max: 2
   },
   {
     name: "3-5",
-    count: 5,
+    count: 0,
     min: 3,
     max: 5
   },
   {
     name: "6-9",
-    count: 5,
+    count: 4,
     min: 6,
     max: 9
   },
   {
     name: "+10",
-    count: 5,
+    count: 2,
     min: 10
   }
 ];
